e74ba701f00cbabd3480d87c85bcbb043a8da848
epogadaev <epogadaev@sinergo.ru>
2018-11-09 14:56:45 +0700

    add @Getter and @Setter

--- common/src/main/java/jdev/rest/consume/RestResponse.java
+++ common/src/main/java/jdev/rest/consume/RestResponse.java
@@ -1,11 +1,14 @@
 package jdev.rest.consume;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Map;
 
 /**
  * Created by epogadaev on 09.11.2018.
  */
 public class RestResponse {
-    public String[] messages;
-    public Map<String, String> result;
+    @Getter @Setter public String[] messages;
+    @Getter @Setter public Map<String, String> result;
 }
